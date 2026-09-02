64ad70b91fe242462d4ab1ff0dfca1f725cd16eb
Jemimah Jemutai <jemutaijemimah@gmail.com>
2026-09-02 09:47:14 +0300

    refactor(firebase): move production web config into source module

---
+++ src/lib/firebaseConfig.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyBz3-3qtnWdqcicw9ATEgn1zorB7WUQ98',
+  authDomain: 'mom-haven.firebaseapp.com',
+  projectId: 'mom-haven',
+  storageBucket: 'mom-haven.firebasestorage.app',
+  messagingSenderId: '306450754584',
+  appId: '1:306450754584:web:5cdc156a69de9b0f43676d',
+};
