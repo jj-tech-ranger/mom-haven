6c2c788b49e51b99b111b446dac368a8fcbfe746
Jemimah Jemutai <jemutaijemimah@gmail.com>
2026-09-02 11:26:42 +0300

    fix: align Firebase web app production config

--- src/lib/firebaseConfig.ts
+++ src/lib/firebaseConfig.ts
@@ -1,8 +1,9 @@
 export const firebaseConfig = {
-  apiKey: 'AIzaSyBz3-3qtnWdqcicw9ATEgn1zorB7WUQ98',
+  apiKey: 'AIzaSyBz3-3qtnWdqcicw9uATEgn1zorB7WUQ98',
   authDomain: 'mom-haven.firebaseapp.com',
   projectId: 'mom-haven',
   storageBucket: 'mom-haven.firebasestorage.app',
   messagingSenderId: '306450754584',
-  appId: '1:306450754584:web:5cdc156a69de9b0f43676d',
+  appId: '1:306450754584:web:e166191c01c67b1743676d',
+  measurementId: 'G-THR90944MY',
 };
